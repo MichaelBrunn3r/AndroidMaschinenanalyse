fee8238fe5dc8774433f315f37a4c2e245bfb1be
OrangeUtan <Oran9eUtan@gmail.com>
2019-12-07 20:23:01 +0100

    Audio Spectrum Chart now shows correct values
    Before fft values were handled incorrectly. Now plotting magnitude

--- app/src/main/java/com/example/mpandroidchartrealtimetest/MainActivity.kt
+++ app/src/main/java/com/example/mpandroidchartrealtimetest/MainActivity.kt
@@ -2,10 +2,10 @@ package com.example.mpandroidchartrealtimetest
 
 import android.Manifest.permission.RECORD_AUDIO
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.YAxis
 import com.github.mikephil.charting.data.Entry
@@ -15,6 +15,8 @@ import com.github.mikephil.charting.formatter.LargeValueFormatter
 import com.paramsen.noise.Noise
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 
 class MainActivity : AppCompatActivity() {
@@ -72,44 +74,42 @@ class MainActivity : AppCompatActivity() {
     private fun configAudioChart() {
         mAudioChart = findViewById(R.id.chartAudio)
 
+        mAudioChart!!.setHardwareAccelerationEnabled(true)
         mAudioChart!!.setTouchEnabled(false)
         mAudioChart!!.isDragEnabled = false
         mAudioChart!!.setScaleEnabled(false)
         mAudioChart!!.setDrawGridBackground(false)
         mAudioChart!!.setPinchZoom(false)
-        mAudioChart!!.setBackgroundColor(Color.WHITE)
-        mAudioChart!!.setDrawBorders(false)
-        mAudioChart!!.setViewPortOffsets(5f,100f,5f,100f)
-        mAudioChart!!.setVisibleXRangeMaximum(SAMPLE_RATE.toFloat())
+        mAudioChart!!.setViewPortOffsets(80f,50f,0f,10f)
+        mAudioChart!!.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.colorPrimaryDark))
 
         // Chart Description
-        mAudioChart!!.description.isEnabled = true
-        mAudioChart!!.description.text = "Real Time Audio Amplitude"
+        mAudioChart!!.description.isEnabled = false
 
         // Chart Data
-        val data = LineData()
-        data.setValueTextColor(Color.BLACK)
-        mAudioChart!!.data = data
+        mAudioChart!!.data =  LineData()
 
         // Legend
         mAudioChart!!.legend.isEnabled = false
 
         // X Axis
         val xAxis = mAudioChart!!.xAxis
-        xAxis.textColor = Color.BLACK
         xAxis.setDrawGridLines(true)
+        xAxis.axisMaximum = SAMPLE_RATE.toFloat()/2
+        xAxis.axisMinimum = 0f
         xAxis.setDrawLabels(true)
-        xAxis.setDrawAxisLine(true)
         xAxis.setAvoidFirstLastClipping(true)
-        xAxis.isEnabled = true
+        xAxis.textColor = ContextCompat.getColor(applicationContext, R.color.colorTextOnPrimary)
         xAxis.valueFormatter = LargeValueFormatter("Hz")
 
         // Y Axis
         val lAxis = mAudioChart!!.axisLeft
-        lAxis.textColor = Color.WHITE
         lAxis.setDrawGridLines(true)
-        lAxis.axisMaximum = 32768f
-        lAxis.axisMinimum = -32768f
+        lAxis.axisMaximum = 32768f*15
+        lAxis.axisMinimum = 0f
+        lAxis.setDrawTopYLabelEntry(true)
+        lAxis.textColor = ContextCompat.getColor(applicationContext, R.color.colorTextOnPrimary)
+        lAxis.valueFormatter = LargeValueFormatter()
 
         // Other Axis
         val rAxis = mAudioChart!!.axisRight
@@ -126,9 +126,8 @@ class MainActivity : AppCompatActivity() {
             var set = createSet()
             data.addDataSet(set)
 
-            for(i in 0 until ffts.size) {
-                //data.addEntry(Entry(set.entryCount.toFloat(), fft), 0)
-                data.addEntry(Entry(fftToFreq(i, SAMPLE_RATE, SAMPLE_SIZE)/2, ffts[i]), 0)
+            for(i in 0 until ffts.size/2) {
+                data.addEntry(Entry(fftToFreq(i, SAMPLE_RATE, SAMPLE_SIZE), fftMagnitude(ffts[i*2],ffts[i*2+1])), 0)
             }
             data.notifyDataChanged()
 
@@ -141,15 +140,20 @@ class MainActivity : AppCompatActivity() {
         return (index * (rate/samples)).toFloat()
     }
 
+    private fun fftMagnitude(real:Float, imaginary:Float):Float {
+        return sqrt(real.pow(2)+imaginary.pow(2))
+    }
+
     private fun createSet(): LineDataSet {
         val set = LineDataSet(null, "Dynamic")
         set.axisDependency = YAxis.AxisDependency.LEFT
         set.lineWidth = 1f
-        set.color = Color.MAGENTA
+        set.color = ContextCompat.getColor(applicationContext, R.color.colorAccent)
         set.isHighlightEnabled = false
         set.setDrawValues(false)
         set.setDrawCircles(false)
-        set.mode = LineDataSet.Mode.LINEAR
+        set.mode = LineDataSet.Mode.CUBIC_BEZIER
+        set.cubicIntensity = 1f
         return set
     }
 
