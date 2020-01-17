ee07b88a3fa0325cbb6fa0cdb89a862b6b307d86
MichaelBrunner <MichaelBrunn3r@gmail.com>
2020-01-17 13:10:39 +0100

    Added default horizontal padding

--- app/src/main/java/com/github/michaelbrunn3r/maschinenanalyse/MonitorFragment.kt
+++ app/src/main/java/com/github/michaelbrunn3r/maschinenanalyse/MonitorFragment.kt
@@ -15,6 +15,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
