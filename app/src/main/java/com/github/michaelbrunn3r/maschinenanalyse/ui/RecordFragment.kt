a1794ad6b2a52156088ede58da5fe049bbf115f5
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-01-24 22:26:50 +0100

    RecordFragment recorded with wrong number of acceleration samples

--- app/src/main/java/com/github/michaelbrunn3r/maschinenanalyse/ui/RecordFragment.kt
+++ app/src/main/java/com/github/michaelbrunn3r/maschinenanalyse/ui/RecordFragment.kt
@@ -79,7 +79,7 @@ class RecordFragment : Fragment() {
 
         val sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val numAccelSamples = preferences.getString("numAccelSamples", Settings.DEFAULT_NUM_ACCEL_SAMPLES.toString())!!.toInt()
-        mVM.accelCfg.value = AccelerationRecordingConfiguration(sensorManager, 512, 9.81f)
+        mVM.accelCfg.value = AccelerationRecordingConfiguration(sensorManager, numAccelSamples, 9.81f)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
