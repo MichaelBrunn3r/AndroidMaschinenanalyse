c29e23d25507f4e9d13aa3253fbbeb82157b67eb
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-01-26 15:17:43 +0100

    Added Util function "getThemeAttr"

--- app/src/main/java/com/github/michaelbrunn3r/maschinenanalyse/util/Utils.kt
+++ app/src/main/java/com/github/michaelbrunn3r/maschinenanalyse/util/Utils.kt
@@ -1,10 +1,20 @@
 package com.github.michaelbrunn3r.maschinenanalyse.util
 
+import android.content.Context
 import android.nfc.FormatException
+import android.util.TypedValue
+
 fun isBitFlagSet(value:Int, flag:Int):Boolean {return value and flag != 0}
 
 fun String.parseRange(): Pair<Int?,Int?> {
     val values = split("..").map { it.toIntOrNull() }
     if(values.size != 2) throw FormatException("Range has to be of format: '[Int]-[Int]'")
     return Pair(values[0],values[1])
+}
+
+fun Context.getThemeAttr(color: String): Int {
+    val resourceId: Int = resources.getIdentifier(color, "attr", packageName)
+    val out = TypedValue()
+    theme.resolveAttribute (resourceId, out, true)
+    return out.data
 }
